42a7f96353a1301f7f4b0eae0de2a72a79449039
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-03-20 22:31:18 +0700

    Update and rename thuvien.java to sachgiaokhoa.java

---
+++ sachgiaokhoa.java
@@ -0,0 +1,61 @@
+package baitapquanlysach;
+ 
+public class SachGiaoKhoa extends Sach {
+    private String tinhTrang;
+    private int number;
+    private double thanhTien;
+ 
+    public SachGiaoKhoa() {
+        super();
+    }
+ 
+    public SachGiaoKhoa(String tinhTrang, int number) {
+        super();
+        this.tinhTrang = tinhTrang;
+        this.number = number;
+    }
+ 
+ 
+    public String getTinhTrang() {
+        return tinhTrang;
+    }
+ 
+ 
+    public void setTinhTrang(String tinhTrang) {
+        this.tinhTrang = tinhTrang;
+    }
+ 
+ 
+    public int getNumber() {
+        return number;
+    }
+ 
+ 
+    public void setNumber(int number) {
+        this.number = number;
+    }
+ 
+    public String tinhTrangSach(int x) {
+        switch (number) {
+            case 0:
+                tinhTrang = "cũ";
+                break;
+            case 1:
+                tinhTrang = "mới";
+                break;
+            default:
+                break;
+        }
+        return tinhTrang;
+    }
+     
+    public void nhapSach() {
+        super.nhapSach();
+        System.out.print("Nhập tình trạng sách (0 - cũ/ 1 - mới): ");
+        number = scanner.nextInt();
+    }
+     
+    public String toString() {
+        return super.toString() + ", tình trạng sách: " + this.tinhTrangSach(number);
+    }
+}
