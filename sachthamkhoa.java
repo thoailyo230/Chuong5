8f8c3cd69986702ede6eeaa3e0dfe584bf72ea5f
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-03-20 22:31:43 +0700

    Create sachthamkhoa.java

---
+++ sachthamkhoa.java
@@ -0,0 +1,34 @@
+package baitapquanlysach;
+ 
+public class SachThamKhao extends Sach {
+    private double thue, thanhTien;
+ 
+    public SachThamKhao() {
+        super();
+    }
+ 
+    public SachThamKhao(double thue) {
+        super();
+        this.thue = thue;
+    }
+ 
+    public double getThue() {
+        return thue;
+    }
+ 
+    public void setThue(double thue) {
+        this.thue = thue;
+    }
+ 
+    public void nhapSach() {
+        super.nhapSach();
+        System.out.print("Nhập thuế: ");
+        thue = scanner.nextDouble();
+    }
+     
+    @Override
+    public String toString() {
+        return super.toString() + ", thuế: " + this.thue;
+    }
+     
+}
